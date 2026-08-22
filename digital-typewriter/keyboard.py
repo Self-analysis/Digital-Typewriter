a9875a96ec2c515bc4cef838f8efd0016769a9aa
Self-analysis <mourtimohamed4@gmail.com>
2026-08-22 17:36:36 +0100

    Add colon key and keep editor cursor focused

--- digital-typewriter/keyboard.py
+++ digital-typewriter/keyboard.py
@@ -17,43 +17,33 @@ class DigitalKeyboard(Gtk.Box):
     
     def __init__(self, editor):
         super().__init__(orientation=Gtk.Orientation.VERTICAL, spacing=4)
-        
         self.editor = editor
-        self.current_lang = 'en'  # 'en' أو 'ar'
+        self.current_lang = 'en'
         self.shift_pressed = False
         self.caps_lock = False
-        
-        # إنشاء لوحة المفاتيح
         self.key_rows = []
         self.key_buttons = {}
         self.setup_keyboard()
-        
-        # تحميل التخطيط الحالي
         self.load_layout()
     
     def setup_keyboard(self):
         """إنشاء صفوف لوحة المفاتيح"""
-        # تعريف صفوف المفاتيح - أضفت @ في الصف الأول بدلاً من `
         rows = [
             ['@', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
             ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-            ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+            ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", ':', 'Enter'],
             ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift'],
             ['Ctrl', 'Alt', 'Space', 'Alt', 'Lang']
         ]
-        
-        # إنشاء كل صف
         for row_keys in rows:
             row_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
             row_box.set_halign(Gtk.Align.FILL)
             row_box.set_hexpand(True)
-            row_box.set_homogeneous(False)  # السماح بأحجام مختلفة للمفاتيح
-            
+            row_box.set_homogeneous(False)
             for key in row_keys:
                 button = self.create_key_button(key, row_keys)
                 row_box.append(button)
                 self.key_buttons[key] = button
-            
             self.append(row_box)
             self.key_rows.append(row_box)
     
@@ -61,173 +51,102 @@ class DigitalKeyboard(Gtk.Box):
         """إنشاء زر للمفتاح"""
         button = Gtk.Button()
         button.add_css_class('key')
-        
-        # تعيين النص والميزات الخاصة
         if key_name in ['Backspace', 'Tab', 'CapsLock', 'Shift', 'Enter', 'Ctrl', 'Alt', 'Space', 'Lang']:
             button.add_css_class('key-special')
-            
             if key_name == 'Space':
                 button.add_css_class('key-space')
                 button.set_label('␣')
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
             elif key_name == 'Lang':
                 button.add_css_class('key-lang')
                 button.set_label('EN / العربية')
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
             elif key_name == 'Backspace':
                 button.set_label('⌫')
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
             elif key_name == 'Enter':
                 button.set_label('↵')
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
             elif key_name == 'CapsLock':
                 button.set_label('⇪')
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
-            elif key_name in ['Shift', 'Tab']:
-                button.set_label(key_name)
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
             else:
                 button.set_label(key_name)
-                button.set_hexpand(True)
-                button.set_halign(Gtk.Align.FILL)
         else:
-            # مفاتيح عادية - نجعلها تتسع بالتساوي
             button.set_label(key_name)
-            button.set_hexpand(True)
-            button.set_halign(Gtk.Align.FILL)
-        
-        # ربط إشارة النقر
+        button.set_hexpand(True)
+        button.set_halign(Gtk.Align.FILL)
         button.connect('clicked', self.on_key_clicked, key_name)
-        
         return button
     
     def load_layout(self):
-        """تحميل تخطيط اللغة الحالية"""
-        if self.current_lang == 'en':
-            self.layout = UK_LAYOUT
-        else:
-            self.layout = ARABIC_LAYOUT
-        
+        self.layout = UK_LAYOUT if self.current_lang == 'en' else ARABIC_LAYOUT
         self.update_keys()
     
     def update_keys(self):
-        """تحديث نص المفاتيح حسب اللغة والحالة"""
         for key_name, button in self.key_buttons.items():
-            # تجاهل المفاتيح الخاصة التي لا تتغير
-            if key_name in ['Backspace', 'Tab', 'CapsLock', 'Shift', 'Enter', 'Ctrl', 'Alt', 'Space', 'Lang']:
+            if key_name in ['Backspace', 'Tab', 'CapsLock', 'Shift', 'Enter', 'Ctrl', 'Alt', 'Space', 'Lang', ':']:
                 continue
-            
             if key_name in self.layout:
-                # الحصول على الحرف حسب الحالة
                 if self.current_lang == 'en':
-                    if self.shift_pressed or self.caps_lock:
-                        char = self.layout[key_name].get('shift', key_name)
-                    else:
-                        char = self.layout[key_name].get('normal', key_name)
+                    char = self.layout[key_name].get('shift' if (self.shift_pressed or self.caps_lock) else 'normal', key_name)
                 else:
-                    # العربية - لا تدعم Shift/CapsLock
                     char = self.layout[key_name]
-                
-                # تحديث النص
                 button.set_label(str(char))
     
     def on_key_clicked(self, button, key_name):
-        """معالجة النقر على المفتاح"""
-        # معالجة المفاتيح الخاصة
         if key_name == 'Backspace':
             self.editor.delete_backward()
-            return
         elif key_name == 'Enter':
             self.editor.insert_newline()
-            return
         elif key_name == 'Tab':
-            self.editor.insert_text('    ')  # 4 مسافات
-            return
+            self.editor.insert_text('    ')
         elif key_name == 'Space':
             self.editor.insert_text(' ')
-            return
         elif key_name == 'Lang':
             self.toggle_language()
-            return
         elif key_name == 'Shift':
             self.toggle_shift()
-            return
         elif key_name == 'CapsLock':
             self.toggle_caps_lock()
-            return
         elif key_name in ['Ctrl', 'Alt']:
-            # مفاتيح تحكم لا تفعل شيئاً في هذا السياق
-            return
-        
-        # إدخال الحرف
-        char = self.get_character(key_name)
-        if char:
-            self.editor.insert_text(char)
-        
-        # إعادة تعيين Shift إذا كان مضغوطاً وليس CapsLock
-        if self.shift_pressed and not self.caps_lock and self.current_lang == 'en':
-            self.shift_pressed = False
-            self.update_keys()
+            pass
+        elif key_name == ':':
+            self.editor.insert_text(':')
+        else:
+            char = self.get_character(key_name)
+            if char:
+                self.editor.insert_text(char)
+            if self.shift_pressed and not self.caps_lock and self.current_lang == 'en':
+                self.shift_pressed = False
+                self.update_keys()
+        # إعادة التركيز إلى محرر النص حتى يبقى مؤشر الكتابة ظاهراً بعد النقر بالماوس.
+        self.editor.grab_focus()
+        self.editor.text_view.set_cursor_visible(True)
     
     def get_character(self, key_name):
-        """الحصول على الحرف المناسب للمفتاح"""
         if key_name in self.layout:
             if self.current_lang == 'en':
-                if self.shift_pressed or self.caps_lock:
-                    return self.layout[key_name].get('shift', key_name)
-                else:
-                    return self.layout[key_name].get('normal', key_name)
-            else:
-                # العربية
-                return self.layout[key_name]
+                return self.layout[key_name].get('shift' if (self.shift_pressed or self.caps_lock) else 'normal', key_name)
+            return self.layout[key_name]
         return None
     
     def toggle_language(self):
-        """التبديل بين اللغات"""
-        if self.current_lang == 'en':
-            self.current_lang = 'ar'
-        else:
-            self.current_lang = 'en'
-        
-        # إعادة تعيين Shift و CapsLock عند تغيير اللغة
+        self.current_lang = 'ar' if self.current_lang == 'en' else 'en'
         self.shift_pressed = False
         self.caps_lock = False
-        
-        # تحديث لوحة المفاتيح
         self.load_layout()
-        
-        # تحديث لغة زر التبديل
         lang_button = self.key_buttons.get('Lang')
         if lang_button:
-            if self.current_lang == 'en':
-                lang_button.set_label('EN / العربية')
-            else:
-                lang_button.set_label('العربية / EN')
-        
-        # إزالة حالة CapsLock من الزر
+            lang_button.set_label('EN / العربية' if self.current_lang == 'en' else 'العربية / EN')
         caps_button = self.key_buttons.get('CapsLock')
         if caps_button:
             caps_button.get_style_context().remove_class('key-active')
     
     def toggle_shift(self):
-        """التبديل بين Shift مضغوط/غير مضغوط"""
         if self.current_lang == 'en':
             self.shift_pressed = not self.shift_pressed
             self.update_keys()
     
     def toggle_caps_lock(self):
-        """التبديل بين CapsLock مفعل/معطل"""
         if self.current_lang == 'en':
             self.caps_lock = not self.caps_lock
             self.update_keys()
-            
-            # تحديث مظهر زر CapsLock
             caps_button = self.key_buttons.get('CapsLock')
             if caps_button:
                 if self.caps_lock:
@@ -236,7 +155,6 @@ class DigitalKeyboard(Gtk.Box):
                     caps_button.get_style_context().remove_class('key-active')
     
     def set_language(self, lang):
-        """تعيين اللغة مباشرة"""
         if lang in ['en', 'ar'] and lang != self.current_lang:
             self.current_lang = lang
             self.load_layout()
